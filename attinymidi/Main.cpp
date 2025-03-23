d873d36f35531b5b4fb77127405312e0e00191b3
wesleyv <wvanderveen@gmail.com>
2025-03-22 21:30:42 -0700

    Added comments to code in preparation for release

--- attinymidi/Main.cpp
+++ attinymidi/Main.cpp
@@ -1,3 +1,31 @@
+/*
+ * Project Name: Attiny-85-Midi-to-Tap-Converter
+ * Description: This is firmware for upgrading the MIDI functionality of the Boss-RC5 looper station, or adding midi functionality to any device that actuates buttons by shorting them to ground.
+ * designed to run on ATTiny85.
+ * 
+ * Copyright (c) 2025 Wesley Van Der Veen
+ * 
+ * This software is released under the MIT License.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE AND HARDWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "Main.h"
 #include "MidiParse.h"
 #include "Pins.h"
@@ -7,15 +35,15 @@
 
 #define MIDIRX 2
 
-#define MIDICH 9
+#define MIDICH 9 //Set this number to match the midi channel that your RC-5 pedal is configured to listen to.
 
 #define AUXTIP 4
 #define AUXRING 3
 #define MAINSWITCH 0
-#define MIDISTATUS 1 // 11 for teensy, 1 for tiny
+#define MIDISTATUS 1 
 
 #define AUXTIPCC 88
-#define AUXRINGCC 89
+#define AUXRINGCC 89 // These variables determine which Midi CC numbers will cause a button press event, and on which button. The RC-5 already listens for 80 through 87, so 88-90 is the recommended choice.
 #define MAINSWITCHCC 90
 
 uint32_t now = 0;
@@ -53,11 +81,11 @@ void onControlChange(uint8_t channel, uint8_t data0, uint8_t data1)
     }
 }
 
-void printBin(uint8_t aByte)
-{
-    for (int8_t aBit = 7; aBit >= 0; aBit--)
-        Serial.write(bitRead(aByte, aBit) ? '1' : '0');
-}
+//void printBin(uint8_t aByte)
+//{
+//    for (int8_t aBit = 7; aBit >= 0; aBit--)
+//        Serial.write(bitRead(aByte, aBit) ? '1' : '0');
+//}
 
 void onError(MidiParserState state, uint8_t b)
 {
@@ -94,6 +122,6 @@ void MainLoop()
     uint8_t b = midiSerial.read();
     // Serial.println("midi serial detected ");
     // printBin(b);
-    Serial.println("");
+    //Serial.println("");
     MidiParserParse(&parser, b);
 }
